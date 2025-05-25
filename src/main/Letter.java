fe571f4ca16706c20ac2317358bbf7b3dc6dc462
Teicir <teicir.bouazizi@hes-so.ch>
2025-05-25 11:14:45 +0200

    ajout constr. lettre

--- src/main/Letter.java
+++ src/main/Letter.java
@@ -1,5 +1,14 @@
 package main;
 
-public class Letter {
-    
+public class Letter extends Item {
+
+    private String content;
+
+
+public Letter (String name, String description, String content) {
+    super(name, description);
+    this.content = content;
+
 }
+} 
+
