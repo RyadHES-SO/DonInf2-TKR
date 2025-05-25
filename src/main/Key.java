00c1ff058a4117f51a9befe28e053b00cb89ced7
Teicir <teicir.bouazizi@hes-so.ch>
2025-05-25 11:25:18 +0200

    ajout consrt key

--- src/main/Key.java
+++ src/main/Key.java
@@ -1,7 +1,12 @@
 package main;
 
-public class Key {
+public class Key extends Item {
+    private Location KeyLocation;
 
 
+public Key (String name, String description, Location KeyLocation) {
+    super(name, description);
+    this.KeyLocation = KeyLocation; 
+ }
     
 }
