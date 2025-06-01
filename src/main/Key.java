74262ee7b63d0f8e92e84d2be2a51b12142c54da
Teicir <teicir.bouazizi@hes-so.ch>
2025-06-01 18:24:27 +0200

    ajout méthode pour utiliser une clé

--- src/main/Key.java
+++ src/main/Key.java
@@ -8,5 +8,10 @@ public Key (String name, String description, Location KeyLocation) {
     super(name, description);
     this.KeyLocation = KeyLocation; 
  }
+
+ public void use() {
+    KeyLocation.unlock();
+    System.out.println("Vous avez déverrouillé : " + KeyLocation.getName());
+}
     
 }
